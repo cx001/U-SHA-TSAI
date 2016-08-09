040ae8abc6915e89b0a3d302c049a5fb4a1221d6
cx001 <b01608055@ntu.edu.tw>
2016-08-09 08:32:30 +0800

    Add files via upload
    display farms and nurs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
             這邊放地圖API 
             從 farms 提取各Farm 經緯度資訊顯示於其上
 
+            
             <div id="map"></div>
             <script>
                 function initMap() {
@@ -21,16 +22,60 @@
                         scrollwheel: true,
                         zoom: 8
                     });
+                    var geocoder = new google.maps.Geocoder();
                     @foreach ($farms as $farm)
                     // Create markers and set its position.
                         var eachFarmLatLng = {lat: {{ $farm->lati }}, lng: {{ $farm->lngi }}};
+                
                         var marker = new google.maps.Marker({
-                    map: map,
-                    position: eachFarmLatLng,
-                    title: '{{ $farm->name }}'
-                }); 
-                @endforeach
+                            map: map,
+                            position: eachFarmLatLng,
+                            title: '{{ $farm->name }}'
+                        }); 
+                    @endforeach
+                             
+                    @foreach ($nurs as $nur)
+                        var address = "{{$nur->地址}}";
+                    // Create markers and set its position.
+                var b;
+                   b= gg(address,map);
+//                       geocoder.geocode({'address': address}, function(results, status) {
+//                           if (status === google.maps.GeocoderStatus.OK) {
+//                               map.setCenter(results[0].geometry.location);
+//                               var marker = new google.maps.Marker({
+//                                   map: map,
+//                                   position: results[0].geometry.location,
+//                                    title: '{{ $nur->育苗場名稱 }}'
+//                               });
+//                           }  else {
+//                               alert('Geocode was not successful for the following reason: ' + status);
+//                           }
+//                       });
+                    @endforeach
                 }
+                
+                
+                function gg(a,m) {
+                     var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({'address': a}, function(results, status) {
+                           if (status === google.maps.GeocoderStatus.OK) {
+                               m.setCenter(results[0].geometry.location);
+                               var markers = new google.maps.Marker({
+                                   map: m,
+                                   position: results[0].geometry.location,
+                                    title: '{{ $nur->育苗場名稱 }}'
+                               });
+                           } else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+                               window.setTimeout(function() {gg(a,m);}, 200);
+                           } else {
+                               alert('Geocode was not successful for the following reason: ' + status);
+                           }
+                       });}
+
+                
+             
+                
+                
             </script>
         </div>
         <div class='span3 sidebar'>
